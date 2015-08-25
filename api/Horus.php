ef5cfb263e702515810ccfe6fd0aa9295226075d
niceperson <niceperson404@gmail.com>
2015-08-25 05:25:06 +0000

    first commit

---
+++ api/Horus.php
@@ -0,0 +1,485 @@
+<?php
+/**
+ * Horus (xpress) - the most powerful tiny php5 framework
+ * 
+ * @package     Horus
+ * @copyright   2015 (c) Horus
+ * @author      Mohammed Al Ashaal
+ * @license     MIT LICENSE
+ * @version     13.0
+ */
+class Horus extends stdClass
+{
+    /**
+     * An instance of this class
+     * @var Horus
+     */
+    protected static $instance;
+
+    /**
+     * Horus configurations container
+     * @var object
+     */
+    public $config;
+
+    /**
+     * Return horus instance
+     * 
+     * @return  Horus
+     */
+    public static function instance()
+    {
+        return static::$instance;
+    }
+
+    /**
+     * Construct
+     * 
+     * @param   array   $config
+     */
+    public function __construct(array $config = [])
+    {
+        // enable output buffering
+        ob_start();
+
+        // configs
+        $this->config = (object) array_merge
+        (
+            [
+                'secure' => false,
+                'base' => '/',
+                'output.handler' => null
+            ],
+
+            $config
+        );
+
+        // query string
+        $this->query = json_decode(json_encode($_GET));
+
+        // cookies
+        $this->cookies = json_decode(json_encode($_COOKIE));
+
+        // request body
+        $this->body = call_user_func(function()
+        {
+            $return = null;
+
+            if ( is_array($a = json_decode(file_get_contents('php://input'), true)) ) {
+                $return = &$a;
+            }
+
+            elseif ( ($a = @simplexml_load_string(file_get_contents('php://input'))) ) {
+                $return = &$a;
+            }
+
+            else {
+                parse_str(file_get_contents('php://input'), $return);
+                $return = json_decode(json_encode($return));
+            }
+
+            return $return;
+        });
+
+        // fix the virtual path
+        $_SERVER['PATH_INFO'] = call_user_func(function($uri)
+        {
+            $path = preg_replace('/\/+/', '/', ('/' . explode('?', $uri, 2)[0] . '/'));
+            $script_name = preg_replace('/\/+/', '/', ('/' . ltrim($_SERVER['SCRIPT_NAME'], '/')));
+            $return = $path;
+
+            if ( stripos($path, $script_name) === 0 )
+                $return = substr($path, strlen($script_name));
+            elseif ( stripos($path, dirname($script_name)) === 0 )
+                $return = substr($path, strlen(dirname($script_name)));
+
+            return preg_replace('/\/+/', '/', ('/' . $return . '/'));
+        }, $_SERVER['REQUEST_URI']);
+
+        // the instance
+        static::$instance = $this;
+    }
+
+    /**
+     * Set http header(s)
+     * 
+     * @param   mixed   $field
+     * @param   string  $value
+     * @return  $this
+     */
+    public function set($field, $value = null)
+    {
+        if ( is_array($field) ) {
+            foreach ( $field as $f => $v )
+                $this->set($f, $v);
+            return $this;
+        }
+
+        $field = str_replace(' ', '-', ucwords(str_replace('-', ' ', strtolower($field))));
+
+        header(sprintf("%s: %s", $field, $value), true);
+
+        return $this;
+    }
+
+    /**
+     * Append http header(s)
+     * 
+     * @param   mixed   $field
+     * @param   string  $value
+     * @return  $this
+     */
+    public function append($field, $value = null)
+    {
+        if ( is_array($field) ) {
+            foreach ( $field as $f => $v )
+                $this->append($f, $v);
+            return $this;
+        }
+
+        $field = str_replace(' ', '-', ucwords(str_replace('-', ' ', strtolower($field))));
+
+        header(sprintf("%s: %s", $field, $value), false);
+
+        return $this;
+    }
+
+    /**
+     * Remove header field(s)
+     * 
+     * @param   mixed   $field
+     * @return  $this
+     */
+    public function remove($field)
+    {
+        foreach ( (array) $field as $f ) {
+            $f = str_replace(' ', '-', ucwords(str_replace('-', ' ', strtolower($f))));
+            header_remove($f);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set/Get the http status code
+     * 
+     * @param   int     $code
+     * @return  $this
+     */
+    public function status($code)
+    {
+        http_response_code((int) $code);
+
+        return $this;
+    }
+
+    /**
+     * Output a message to the browser
+     * 
+     * @param   string     $message
+     * @return  $this
+     */
+    public function send($message)
+    {
+        echo $message;
+
+        return $this;
+    }
+
+    /**
+     * Output json to the browser
+     * 
+     * @param   mixed   $message
+     * @return  $this
+     */
+    public function json($message)
+    {
+        $this->set('content-type', 'application/json; charset=UTF-8')->send(json_encode($message));
+        return $this;
+    }
+
+    /**
+     * Output json to the browser
+     * 
+     * @param   string  $message
+     * @return  $this
+     */
+    public function jsonp($message, $cb = 'cb')
+    {
+        $this->set('content-type', 'application/javascript; charset=UTF-8')->send(sprintf('%s(%s)', $cb, json_encode($message)));
+        return $this;
+    }
+
+    /**
+     * Clear the output
+     * 
+     * @return  $this
+     */
+    public function clear()
+    {
+        ob_clean();
+        return $this;
+    }
+
+    /**
+     * Send a cookie to the borwser
+     * 
+     * @param   string  $name
+     * @param   string  $value
+     * @param   array   $options
+     * @return  $this
+     */
+    public function cookie($name, $value = "", array $options = [])
+    {
+        $options    =   array_merge(
+        [
+            'domain'    =>  null,
+            'path'      =>  '/',
+            'expires'   =>  0,
+            'secure'    =>  (bool) $this->config->secure,
+            'httpOnly'  =>  true
+        ], array_change_key_case($options, CASE_LOWER));
+
+        setcookie (
+            $name,
+            $value,
+            (int) $options['expires'], 
+            $options['path'],
+            $options['domain'],
+            $options['secure'], 
+            $options['httpOnly']
+        );
+
+        return $this;
+    }
+
+    /**
+     * Output the specified filename(s) to the browser and optionally pass vars to it/them
+     * 
+     * @param   string  $filename
+     * @param   array   $scope
+     * @return  $this
+     */
+    public function render($filename, array $scope = [])
+    {
+        extract($scope, EXTR_OVERWRITE|EXTR_REFS);
+
+        foreach ( (array) $filename as $_____ ) {
+            if ( is_file($_____) ) {
+                require $_____;
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Redirect to the specified '$target'
+     * 
+     * @param   string  $target
+     * @param   bool    $permanent
+     * @return  $this
+     */
+    public function redirect($target, $permanent = false)
+    {
+        $code = $permanent ? 301 : 302;
+
+        $this->set('location', $target)->clear()->status($code)->end();
+
+        return $this;
+    }
+
+    /**
+     * Ends the application with data, http headers and status code
+     * 
+     * @param   string  $data
+     * @param   int     $status
+     * @param   array   $headers
+     * @return  void
+     */
+    public function end($data = null, $status = null, array $headers = [])
+    {
+        if ( null !== $status ) {
+            $this->status($status);
+        }
+
+        if ( [] !== $headers ) {
+            $this->set($headers);
+        }
+
+        if ( null !== $data ) {
+            $this->send($data);
+        }
+
+        $output = ob_get_clean();
+
+        if ( is_callable($this->config->{'output.handler'}) ) {
+            $output = call_user_func($this->config->{'output.handler'}, $output);
+        }
+
+        die($output);
+    }
+
+    /**
+     * Listen on the requested uri
+     * 
+     * @param   string      $pattern
+     * @param   Closure     $listener
+     * @return  $this
+     */
+    public function on($pattern, Closure $listener)
+    {
+        $listener = $listener->bindTo($this);
+        $parts = explode(' ', $pattern, 2);
+
+        if ( ! isset($parts[1]) ) {
+            $method = $_SERVER['REQUEST_METHOD'];
+            $pattern = $parts[0];
+        }
+
+        else {
+            $method = $parts[0];
+            $pattern = $parts[1];
+        }
+
+        $method = explode('|', strtolower($method));
+        $pattern = "~^" . (preg_replace('/\/+/', '/', ('/' . str_replace(['/:*', '/:?'], ['/?(.*)', '/([^\/]+)'], $pattern) . '/'))) . "$~";
+
+        if ( in_array(strtolower($_SERVER['REQUEST_METHOD']), $method) && preg_match($pattern, $_SERVER['PATH_INFO'], $args) ) {
+            array_shift($args);
+            call_user_func_array($listener, $args);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Listen on hostname and handle custome virtual-hosts
+     * 
+     * @param   string      $pattern
+     * @param   Closure     $listener
+     * @return  $this
+     */
+    public function vhost($pattern, Closure $listener)
+    {
+        $listener = $listener->bindTo($this);
+        $host = (empty($_SERVER['SERVER_NAME']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME']);
+
+        if ( preg_match("/^{$pattern}$/", $host, $m) ) {
+            array_shift($m);
+            call_user_func_array($listener, $m);            
+         }
+
+        return $this;
+    }
+
+    /**
+     * Return an url for a local path
+     * 
+     * @param   string  $path
+     * @param   string  $host
+     * @return  string
+     */
+    public function url($path = '', $host = null)
+    {
+        if ( empty($host) ) {
+            $host = (empty($_SERVER['SERVER_NAME']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME']);
+        }        
+
+        return sprintf
+        (
+            // the required template
+            "%s://%s/%s",
+
+            // schema 'http/https'
+            ($this->config->secure ? 'https' : 'http'),
+
+            // host
+            $host,
+
+            // the required path
+            ltrim($path, '/')
+        );
+    }
+
+    /**
+     * Return an url for a local path
+     * 
+     * @param   string  $path
+     * @param   string  $host
+     * @return  string
+     */
+    public function route($path = '', $host = null)
+    {
+        if ( empty($host) ) {
+            $host = (empty($_SERVER['SERVER_NAME']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME']);
+        }
+
+        return sprintf
+        ( 
+            // the required template
+            "%s://%s%s%s",
+
+            // schema 'http/https'
+            ($this->config->secure ? 'https' : 'http'),
+
+            // host
+            $host,
+
+            // the base
+            preg_replace('/\/+/', '/', ('/' . $this->config->base . '/')),
+
+            // the required path
+            ltrim($path, '/')
+        );
+    }
+
+    /**
+     * Make class in the specified dir(s) be autoloaded
+     * 
+     * @param   string|array    $dir
+     * @return  $this
+     */
+    public function autoload($dir)
+    {
+        foreach ( (array) $dir as $d )
+        {
+            spl_autoload_register(function($class) use($d)
+            {
+                $ds = DIRECTORY_SEPARATOR;
+                $class = trim(str_replace(['\\'], $ds, $class), '\\_/');
+                $name = basename($class);
+                $prop = [ '.php', $ds . $name . '.php' ];
+
+                foreach ( $prop as $p )
+                {
+                    if ( is_file($file = $d . $ds. $class . $p) )
+                    {
+                        include_once($file);
+                        return ;
+                    }
+                }
+            });
+        }
+
+        return $this;
+    }
+
+    /** @ignore */
+    public function __call($name, $args)
+    {
+        return  isset($this->{$name}) && is_callable($this->{$name})
+                ? call_user_func_array($this->{$name}, $args)
+                : null
+        ;
+    }
+
+    /** @ignore */
+    public function __set($k, $v)
+    {
+        if ( $v instanceof Closure )
+            $this->{$k} = $v->bindTo($this);
+        else
+            $this->{$k} = $v;
+    }
+}
---
+++ api/index.php
@@ -0,0 +1,352 @@
+<?php
+
+//load routing library
+require('Horus.php');
+
+// instantiate app object
+$app = new Horus();
+
+// function to create db connection
+$app->dbc = function(){
+	$dbuser = 'root';
+	$dbpass = '';
+	try {
+	    $dbh = new PDO("mysql:host=localhost;dbname=dspms", $dbuser, $dbpass);
+	    return $dbh;
+	}catch(PDOException $e){
+	    echo $e->getMessage();
+	}
+};
+
+//function to validate access
+$app->auth = function($token,$uid){
+
+	// query db for token validation
+	$sql = 'SELECT id FROM user WHERE access_token=:token AND id=:id LIMIT 1';
+	// bind parameter
+	$prm = [
+		'id' => $uid,
+		':token' => $token
+	];
+
+	$dbh = $this->dbc();
+	$qry = $dbh->prepare($sql);
+	$qry->execute($prm);
+	$res = $qry->fetch(PDO::FETCH_ASSOC);
+
+	if(empty($res)){
+		return false;
+	}else{
+		return true;
+	}
+};
+
+//function to output error
+$app->out = function($msg,$code){
+	if(is_array($msg)){
+		$this->end(json_encode($msg),$code);
+	}else{
+		$this->end(json_encode(['msg' => $msg]),$code);
+	}
+};
+
+//api for user login ------->  "/api/login"
+$app->on('POST /login', function(){
+	$username = $this->body->username;
+	$password = $this->body->password;
+
+	// db sql query to check user and password
+	$sql = 'SELECT id FROM user WHERE username=:uname AND password=:upass LIMIT 1';
+	// query parameter
+	$prm = [
+		':uname' => $username,
+		':upass' => $password
+	];
+	// create db connection and executing query
+	$dbh = $this->dbc();
+	$qry = $dbh->prepare($sql);
+	$qry->execute($prm);
+	$res = $qry->fetch(PDO::FETCH_ASSOC);
+
+	if(!empty($res)) {
+		$token = md5(time());
+		// update query to append access token
+		$sql = 'UPDATE user SET access_token=:token WHERE id=:id';
+		// query parameter
+		$prm = [
+			':id' => $res['id'],
+			':token' => $token
+		];
+		// create db connection and executing query
+		$dbh = $this->dbc();
+		$qry = $dbh->prepare($sql);
+		$qry->execute($prm);
+	}
+
+	// creating json output
+	$out = [
+		'id' => $res['id'],
+		'token' => $token
+	];
+
+	// send output
+	$this->out($out,202);
+});
+
+//api to get all user ------->  "/api/user"
+$app->on('GET /user', function(){
+	$token = (isset($this->query->token)) ? $this->query->token: $this->out('token is required',400);
+	$uid = (isset($this->query->uid)) ? $this->query->uid: $this->out('uid is required',400);
+	if(!$this->auth($token,$uid) && $uid != 1){
+		$this->out('access unauthorized',400);
+	}else{
+		//select all user from db
+		$sql = 'SELECT id,username FROM user WHERE id>1';
+		// connect to db and execute
+		$dbh = $this->dbc();
+		$qry = $dbh->prepare($sql);
+		$qry->execute();
+
+		//get results
+		$res = $qry->fetchAll(PDO::FETCH_ASSOC);
+
+		// send output
+		$this->out($res,200);
+	}
+});
+
+//api for add user ------->  "/api/user"
+$app->on('POST /user', function(){
+	$token = (isset($this->query->token)) ? $this->query->token: $this->out('token is required',400);
+	$uid = (isset($this->query->uid)) ? $this->query->uid: $this->out('uid is required',400);
+	if(!$this->auth($token,$uid)){
+		$this->out('access unauthorized',400);
+	}else{
+		$username = $this->body->username;
+		$password = $this->body->password;
+
+		//check if user already existed
+		$sql = 'SELECT id FROM user WHERE username=:uname LIMIT 1';
+		// query parameter
+		$prm = [
+			':uname' => $username
+		];
+		// create db connection and executing query
+		$dbh = $this->dbc();
+		$qry = $dbh->prepare($sql);
+		$qry->execute($prm);
+		$res = $qry->fetch(PDO::FETCH_ASSOC);
+
+		if(!empty($res)) {
+			$this->out('username already taken.',200);
+		}else{
+			// db sql query to insert user and password
+			$sql = 'INSERT INTO user (username, password) VALUES (:uname, :upass) ';
+			// query parameter
+			$prm = [
+				':uname' => $username,
+				':upass' => $password
+			];
+			// create db connection and executing query
+			$dbh = $this->dbc();
+			$qry = $dbh->prepare($sql);
+			$qry->execute($prm);
+
+			// send output
+			$this->end('new user created!',201);
+		}
+	}
+});
+
+//api for update user ------->  "/api/user/{id}"
+$app->on('POST /user/([0-9]+)', function($id){
+	$token = (isset($this->query->token)) ? $this->query->token: $this->out('token is required',400);
+	$uid = (isset($this->query->uid)) ? $this->query->uid: $this->out('uid is required',400);
+	$password = (isset($this->body->password) && !empty($this->body->password)) ? $this->body->password: $this->out('password is required',400);
+
+	if(!$this->auth($token,$uid)){
+		$this->out('access unauthorized',400);
+	}else{
+		if($uid == 1) {
+			// db query to update user for admin
+			$sql = 'UPDATE user SET password=:upass WHERE id=:id';
+			// bind parameter
+			$prm = [
+				':id' => $id,
+				':upass' => $password
+			];
+		}else{
+			// db query to update user for the user itself
+			$sql = 'UPDATE user SET password=:upass WHERE id=:id AND access_token=:token';
+			// bind parameter
+			$prm = [
+				':id' => $id,
+				':upass' => $password,
+				':token' => $token
+			];
+		}
+
+		// connect to db and execute query
+		$dbh = $this->dbc();
+		$qry = $dbh->prepare($sql);
+		$qry->execute($prm);
+
+		// send output
+		$this->out('password update successful!',202);
+	}
+});
+
+//api for delete user ------->  "/api/user/{id}"
+$app->on('DELETE /user/([0-9]+)', function($id){
+	$token = (isset($this->query->token)) ? $this->query->token: $this->out('token is required',400);
+	$uid = (isset($this->query->uid)) ? $this->query->uid: $this->out('uid is required',400);
+	if(!$this->auth($token,$uid) || $uid != 1){
+		$this->out('access unauthorized',400);
+	}elseif($id == 1){
+		$this->out('Cant delete default user',401);
+	}else{
+		//db query to delete user
+		$sql = 'DELETE FROM user WHERE id=:id';
+		//set parameter
+		$prm = [
+			':id'=>$id
+		];
+		//connect db and execute
+		$dbh = $this->dbc();
+		$qry = $dbh->prepare($sql);
+		$qry->execute($prm);
+
+		// send output
+		$this->out('user deleted!',202);
+	}
+});
+
+
+
+//api for find parcel ------->  "/api/parcel/find"
+$app->on('GET /parcel/find/{}', function(){
+	
+});
+
+//api for list parcel ------->  "/api/parcel"
+$app->on('GET /parcel/student/{}', function(){
+	
+});
+
+//api for add parcel ------->  "/api/parcel"
+$app->on('POST /parcel', function(){
+	$token = (isset($this->query->token)) ? $this->query->token: $this->out('token is required',400);
+	$uid = (isset($this->query->uid)) ? $this->query->uid: $this->out('uid is required',400);
+	if(!$this->auth($token,$uid) || $uid != 1){
+		$this->out('access unauthorized',400);
+	}else{
+		$parcel = (isset($this->body->parcel)) ? $this->body->parcel : $this->out('parcel is required',400);
+		$data = json_decode($parcel);	
+
+		//check if user already existed
+		$sql = 'SELECT id FROM parcel WHERE parcel_id=:parcel_id LIMIT 1';
+		// query parameter
+		$prm = [
+			':parcel_id' => $data->parcel_id
+		];
+		// create db connection and executing query
+		$dbh = $this->dbc();
+		$qry = $dbh->prepare($sql);
+		$qry->execute($prm);
+		$res = $qry->fetch(PDO::FETCH_ASSOC);
+
+		if(!empty($res)) {
+			$this->out('parcel already added',200);
+		}else{
+			// db sql query to add parcel
+			$sql = 'INSERT INTO parcel (student_id, student_name, parcel_id, date_in, status) VALUES (:sid, :sname, :parcel, :date_in, :status) ';
+			// query parameter
+			$prm = [
+				':sid' => $data->student_id,
+				':sname' => $data->student_name,
+				':parcel' => $data->parcel_id,
+				':date_in' => date('Y-m-d'),
+				':status' => ParcelStatus::RECEIVED
+			];
+
+			// create db connection and executing query
+			$dbh = $this->dbc();
+			$qry = $dbh->prepare($sql);
+			$qry->execute($prm);
+
+			// send output
+			$this->end('parcel added!',201);
+		}
+	}
+});
+
+//api for update parcel ------->  "/api/parcel/{id}"
+$app->on('POST /parcel/([0-9]+)', function($pid) {
+	$token = (isset($this->query->token)) ? $this->query->token : $this->out('token is required',400);
+	$uid = (isset($this->query->uid)) ? $this->query->uid : $this->out('uid is required',400);
+	$parcel = (isset($this->body->parcel)) ? $this->body->parcel : $this->out('parcel is required',400);
+	$data = json_decode($parcel);
+	if(!$this->auth($token,$uid) || $uid != 1){
+		$this->out('access unauthorized',400);
+	}else{
+		$prm = [];
+		$sql = 'UPDATE parcel SET ';
+		foreach($data as $k => $v) {
+			$sql.= $k.'=:'.$k.', ';
+			$prm[':'.$k] = $v;
+		}
+		$sql = substr($sql, 0, -2);  
+		$sql.= ' WHERE id=:id';
+		$prm[':id'] = (int)$pid;
+
+		// connect to db and execute query
+		$dbh = $this->dbc();
+		$qry = $dbh->prepare($sql);
+		$qry->execute($prm);
+
+		//send output
+		$this->out('parcel updated',202);
+	}
+});
+
+//api for delete parcel ------->  "/api/parcel/{id}"
+$app->on('DELETE /parcel/([0-9]+)', function($pid){
+	$token = (isset($this->query->token)) ? $this->query->token: $this->out('token is required',400);
+	$uid = (isset($this->query->uid)) ? $this->query->uid: $this->out('uid is required',400);
+	if(!$this->auth($token,$uid) || $uid != 1){
+		$this->out('access unauthorized',400);
+	}elseif($id == 1){
+		$this->out('Cant delete default user',401);
+	}else{
+		//db query to delete user
+		$sql = 'DELETE FROM parcel WHERE id=:id';
+		//set parameter
+		$prm = [
+			':id'=>$pid
+		];
+		//connect db and execute
+		$dbh = $this->dbc();
+		$qry = $dbh->prepare($sql);
+		$qry->execute($prm);
+
+		//send output
+		$this->out('parcel deleted!',202);
+	}
+});
+
+//default route 
+$app->on('/:*', function(){
+	$out = json_encode([
+		'err' => true,
+		'msg' => 'cannot find any matched route !'
+	]);
+    $this->end($out,404);
+});
+
+
+// constant for parcel status
+class ParcelStatus{
+	const UNKNOWN     = 0;
+	const RECEIVED    = 1;
+	const CLAIMED     = 2;
+}
